3ec8fff8e6044461131491e25d7f1f8777c60677
Mirzohid Dilshodov <md20021203@gmail.com>
2022-04-10 13:21:12 +0500

    little UI changes

--- app/src/main/java/com/wonmirzo/activity/CardActivity.kt
+++ app/src/main/java/com/wonmirzo/activity/CardActivity.kt
@@ -36,10 +36,6 @@ class CardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
         setContentView(R.layout.activity_card)
         initViews()
     }
--- app/src/main/java/com/wonmirzo/activity/MainActivity.kt
+++ app/src/main/java/com/wonmirzo/activity/MainActivity.kt
@@ -35,10 +35,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
         setContentView(R.layout.activity_main)
         initViews()
     }
